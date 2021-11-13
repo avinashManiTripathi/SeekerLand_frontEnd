2189e47c0817a33bc390c4e3c826e1be138e59f5
Avinash <v-avinash.tripathi@monsterindia.com>
2021-11-13 22:31:48 +0530

    added signup and navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Layout/Header/Header";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./PublicPages/Home/Home";
-import Footer from "./Layout/Footer/Footer";
-import Jobs from "./PublicPages/Jobs/Jobs";
-import JobsDetails from "./PublicPages/Jobs/JobsDetails";
-import Recruiter from "./PublicPages/Recruiter/Recruiter";
-import SignIn from "./Authentication/SignIn";
-import MyAccount from "./Seeker/MyAccount";
-import SavedJobs from "./Seeker/SavedJobs";
-import AppliedJobs from "./Seeker/AppliedJobs";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Layout/Header/Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './PublicPages/Home/Home';
+import Footer from './Layout/Footer/Footer';
+import Jobs from './PublicPages/Jobs/Jobs';
+import JobsDetails from './PublicPages/Jobs/JobsDetails';
+import Recruiter from './PublicPages/Recruiter/Recruiter';
+import SignIn from './Authentication/SignIn';
+import MyAccount from './Seeker/MyAccount';
+import SavedJobs from './Seeker/SavedJobs';
+import AppliedJobs from './Seeker/AppliedJobs';
+import Header2 from './Layout/Header/Header2';
+import SignUp from './Authentication/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header></Header>
-      <Route exact path="/" component={Home}></Route>
-      <Route exact path="/signin" component={SignIn}></Route>
-      <Route path="/jobs" component={Jobs}></Route>
-      <Route path="/job-details" component={JobsDetails}></Route>
-      <Route path="/recruiters" component={Recruiter}></Route>
-      <Route path="/my-account" component={MyAccount}></Route>
-      <Route path="/applied_jobs" component={AppliedJobs}></Route>
-      <Route path="/saved_jobs" component={SavedJobs}></Route>
+      {/* <Header></Header> */}
+      <Header2></Header2>
+      <Route exact path='/' component={Home}></Route>
+      <Route path='/signup' component={SignUp}></Route>
+      <Route exact path='/signin' component={SignIn}></Route>
+      <Route path='/jobs' component={Jobs}></Route>
+      <Route path='/job-details' component={JobsDetails}></Route>
+      <Route path='/recruiters' component={Recruiter}></Route>
+      <Route path='/my-account' component={MyAccount}></Route>
+      <Route path='/applied_jobs' component={AppliedJobs}></Route>
+      <Route path='/saved_jobs' component={SavedJobs}></Route>
       <Footer />
     </BrowserRouter>
   );
--- src/Authentication/SignIn.js
+++ src/Authentication/SignIn.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Col, Container, Form, Row, Button, Spinner } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-import "./index.css"
+import React, { useState } from 'react';
+import { Col, Container, Form, Row, Button, Spinner } from 'react-bootstrap';
+import { Link, useHistory } from 'react-router-dom';
+import './index.css';
 
 const SignIn = (props) => {
   const history = useHistory();
@@ -16,38 +16,38 @@ const SignIn = (props) => {
   };
 
   const handleLogin = () => {
-    history.push("my-account");
+    localStorage.setItem('isLoggedIn', true);
+    history.push('my-account');
   };
   return (
-    <Container >
-      <Row >
+    <Container>
+      <Row>
         <Col xs={12} md={6}>
-          <div className="signin-sidebar-content "
-        >
-            <h4 className="mb-4">
+          <div className='signin-sidebar-content '>
+            <h4 className='mb-4'>
               Create, <span> Build, Share </span>& Find Better Candidates
             </h4>
-            <div className="d-flex">
-              <span>&#9830;</span>{" "}
+            <div className='d-flex'>
+              <span>&#9830;</span>{' '}
               <p>
                 It's not only FREE but also a great way to connect with job
                 seekers
               </p>
             </div>
-            <div className="d-flex">
-              <span>&#9830;</span>{" "}
+            <div className='d-flex'>
+              <span>&#9830;</span>{' '}
               <p>Build followers network to amplify your Reach </p>
             </div>
-            <div className="d-flex">
-              <span>&#9830;</span>{" "}
+            <div className='d-flex'>
+              <span>&#9830;</span>{' '}
               <p>Share job postings with your Followers </p>
             </div>
-            <div className="d-flex">
-              <span>&#9830;</span>{" "}
+            <div className='d-flex'>
+              <span>&#9830;</span>{' '}
               <p>Build your very own Personal Brand as a recruiter</p>
             </div>
-            <div className="d-flex">
-              <span>&#9830;</span>{" "}
+            <div className='d-flex'>
+              <span>&#9830;</span>{' '}
               <p>
                 Create a Ready to Hire talent pipeline for your sourcing needs
               </p>
@@ -55,40 +55,39 @@ const SignIn = (props) => {
           </div>
         </Col>
         <Col xs={6} md={6}>
-          <div className="signin-sidebar-form bg-white shadow p-4">
-            <div className="form_title">
+          <div className='signin-sidebar-form bg-white shadow p-4'>
+            <div className='form_title'>
               <h2>Sign In</h2>
             </div>
             <Form onSubmit={handleLogin}>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className='mb-3' controlId='formBasicEmail'>
                 <Form.Control
-                  type="email"
-                  className="br-0"
-                  placeholder="Enter Registered Phone Number"
+                  type='email'
+                  className='br-0'
+                  placeholder='Enter Registered Phone Number'
                 />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <div className="d-flex">
+              <Form.Group className='mb-3' controlId='formBasicPassword'>
+                <div className='d-flex'>
                   <Form.Control
-                    type="password"
-                    className="br-0"
-                    placeholder="Enter Generated OTP"
+                    type='password'
+                    className='br-0'
+                    placeholder='Enter Generated OTP'
                   />
                   <Button
-                    variant="danger br-0 form-btn"
-                    onClick={handleSendOTP}
-                  >
+                    variant='danger br-0 form-btn'
+                    onClick={handleSendOTP}>
                     {state.buttonLoader ? (
-                      <div className="d-flex">
+                      <div className='d-flex'>
                         <span>GenerateOTP </span>
                         <Spinner
-                          as="span"
-                          className="m-1"
-                          animation="border"
-                          size="sm"
-                          role="status"
-                          aria-hidden="true"
+                          as='span'
+                          className='m-1'
+                          animation='border'
+                          size='sm'
+                          role='status'
+                          aria-hidden='true'
                         />
                       </div>
                     ) : (
@@ -96,23 +95,23 @@ const SignIn = (props) => {
                     )}
                   </Button>
                 </div>
-                <Form.Text className="text-muted form-inner-text-danger">
+                <Form.Text className='text-muted form-inner-text-danger'>
                   We'll never share your OTP with anyone else.
                 </Form.Text>
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Group className='mb-3' controlId='formBasicCheckbox'>
                 <Form.Check
-                  type="checkbox"
-                  className="text-muted form-inner-text-danger"
-                  label="Remember me"
+                  type='checkbox'
+                  className='text-muted form-inner-text-danger'
+                  label='Remember me'
                 />
               </Form.Group>
-              <div className="d-flex">
-                <Button variant="danger br-0 form-btn" type="submit">
+              <div className='d-flex'>
+                <Button variant='danger br-0 form-btn' type='submit'>
                   Sign In
                 </Button>
-                <Form.Text className="m-3 text-muted form-inner-text-danger">
-                  Don't have an account ? <Link to="signin">Sign Up</Link>
+                <Form.Text className='m-3 text-muted form-inner-text-danger'>
+                  Don't have an account ? <Link to='/signup'>Sign Up</Link>
                 </Form.Text>
               </div>
             </Form>
